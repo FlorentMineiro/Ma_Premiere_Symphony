363cc762d50ec2485a4f9f480360a1e0d7f3ccff
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-11-04 21:56:49 +0100

    Créations de routes yaml xml et php

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+// config/routes.php
+use App\Controller\BlogController;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes): void {
+    $routes->add('blog_list', '/blog')
+        // the controller value has the format [controller_class, method_name]
+        ->controller([BlogController::class, 'list'])
+
+        // if the action is implemented as the __invoke() method of the
+        // controller class, you can skip the 'method_name' part:
+        // ->controller(BlogController::class)
+    ;
+};
